2f7a58bc08af21952ec072ffcf308d0b942968c1
Quoc Hoang <quochoangddt@gmail.com>
2015-03-21 01:20:45 +0700

    #2 Add optinal format suffixes to URLs

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -7,7 +7,7 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format = None):
     """
     List all snippets, or create a new snippet.
     """
@@ -24,7 +24,7 @@ def snippet_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format = None):
     """
     Retrieve, update or delete a snippet instance.
     """
