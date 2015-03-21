6148f1dd2bff1f16573df4d73f7865526418f363
Quoc Hoang <quochoangddt@gmail.com>
2015-03-21 09:46:19 +0700

    #4 Add login to the Browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
 )
+
+# add a pattern to include the login and logout views for the browsable API.
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
