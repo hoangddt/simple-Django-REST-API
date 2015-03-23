887f856fa6e54182de45a30a2d6968aa4f7c7538
Hoang Truong <quochoangddt@gmail.com>
2015-03-23 23:59:32 +0700

    Add rest_framework_swagger to document the API

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'snippets',
+    'rest_framework_swagger',
 )
 
 MIDDLEWARE_CLASSES = (
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,6 +14,7 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
 
